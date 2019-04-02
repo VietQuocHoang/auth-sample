7934f73f312adde15eb68f66a229d623a76011d2
Viet Hoang <viet.hoang@lucidfoundry.com>
2019-04-02 18:15:34 +0700

    - Initial data for permission

--- test_auth/models.py
+++ test_auth/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 from django.contrib.auth import models as auth_models
 from django.contrib.auth import get_user_model
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 UserModel = get_user_model()
 
 
-class CustomGroup(auth_models.Group):
-    team = models.ForeignKey('Team', null=True, on_delete=models.DO_NOTHING)
-    permissions = models.ManyToManyField('GroupPermisison')
-
-
-class GroupPermission(models.Model):
+class TeamPermission(models.Model):
     name = models.TextField()
+
+
+class Group(models.Model):
+    name = models.CharField(_('name'), max_length=80, unique=True)
+
+    team = models.ForeignKey('Team', null=True, on_delete=models.DO_NOTHING)
+    team_permissions = models.ManyToManyField(TeamPermission, blank=True)
+
+    objects = auth_models.GroupManager()
+
+    class Meta:
+        verbose_name = _('group')
+        verbose_name_plural = _('groups')
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.name,)
 
 
 class Team(models.Model):
